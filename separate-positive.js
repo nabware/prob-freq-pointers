639d166027ed0311853a783b70fc5a087c981b23
Nabeel Rana <nabwaregroup@gmail.com>
2023-09-15 17:25:09 -0700

    Started seperatePositive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,16 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
+
+/** Takes in an array of non-zero numbers
+ *
+ * Returns the same array with negative numbers on the left
+ * and positive numbers on the right.
+ */
+
 function separatePositive() {
+  let a = [1, 2, -4, 7, -9];
+  // [1, 2, 7, -4, -9]
+  a[2] =
+
+  a[1] = 1;
 }
